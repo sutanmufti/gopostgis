15b5eb27770da22e87d19042d93e7a4f7abce8a8
sutan mufti <upsutan@gmail.com>
2025-09-21 10:10:15 +0100

    Point Z

--- geometry/pointz.go
+++ geometry/pointz.go
@@ -1,10 +1,15 @@
 package GeometryConstructor
 
+import "fmt"
+
 type PointZ struct {
 	coordinates Coordinate
 }
 
-func (p *PointZ) WKT() string          { return "" }
+func (p *PointZ) WKT() string {
+	c := p.coordinates
+	return fmt.Sprintf("POINT Z (%s)", c.String())
+}
 func (p *PointZ) GeoJSON() string      { return "" }
 func (p *PointZ) GeometryType() string { return "POINT Z" }
-func (p *PointZ) String() string       { return "POINT Z" }
+func (p *PointZ) String() string       { return p.WKT() }
