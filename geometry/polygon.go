9d4e089366606f086ce6c3975b2d99d460fa3dd3
sutan mufti <upsutan@gmail.com>
2025-09-21 20:44:49 +0100

    polygon satisfies interface

--- geometry/polygon.go
+++ geometry/polygon.go
@@ -2,7 +2,13 @@ package GeometryConstructor
 
 type Polygon struct {
 	coordinates [][]Position
+	GeomType    string
 }
 
-func (p *Polygon) WKT()     {}
-func (p *Polygon) GeoJSON() {}
+func (p *Polygon) WKT() string {
+	return ""
+}
+func (p *Polygon) GeoJSON() string           { return "" }
+func (p *Polygon) GeometryType() string      { return p.GeomType }
+func (p *Polygon) String() string            { return p.WKT() }
+func (p *Polygon) GetCoordinate() Coordinate { return nil }
