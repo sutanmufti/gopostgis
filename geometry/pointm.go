f827a02201549c62dca9bdefe8d89e38ceb5187c
sutan mufti <upsutan@gmail.com>
2025-09-21 10:11:24 +0100

    pointzm and pointm

--- geometry/pointm.go
+++ geometry/pointm.go
@@ -1,10 +1,15 @@
 package GeometryConstructor
 
+import "fmt"
+
 type PointM struct {
 	coordinates Coordinate
 }
 
-func (p *PointM) WKT() string          { return "" }
+func (p *PointM) WKT() string {
+	c := p.coordinates
+	return fmt.Sprintf("POINT M (%s)", c.String())
+}
 func (p *PointM) GeoJSON() string      { return "" }
 func (p *PointM) GeometryType() string { return "POINT M" }
-func (p *PointM) String() string       { return "POINT M" }
+func (p *PointM) String() string       { return p.WKT() }
--- geometry/pointzm.go
+++ geometry/pointzm.go
@@ -1,10 +1,15 @@
 package GeometryConstructor
 
+import "fmt"
+
 type PointZM struct {
 	coordinates Coordinate
 }
 
-func (p *PointZM) WKT() string          { return "" }
+func (p *PointZM) WKT() string {
+	c := p.coordinates
+	return fmt.Sprintf("POINT ZM (%s)", c.String())
+}
 func (p *PointZM) GeoJSON() string      { return "" }
 func (p *PointZM) GeometryType() string { return "POINT ZM" }
-func (p *PointZM) String() string       { return "POINT ZM" }
+func (p *PointZM) String() string       { return p.WKT() }
